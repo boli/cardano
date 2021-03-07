ce5b242607ec7c7ba6e6200150a92e1a3d5dadbe
Boli <188146+boli@users.noreply.github.com>
2021-03-07 11:50:26 +0000

    fix names

---
+++ check_cardano_latestblock.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use LWP::Simple;
+
+my $DEBUG;
+
+#curl -s https://blockchair.com/cardano|grep "Latest block" -m1|sed 's/.*Latest/Latest/'|sed 's/<\/a>.*//'|sed 's/.*<.*>//'
+
+my $metricsurl="http://127.0.0.1:12798/metrics";
+my $warn = 7;
+my $crit = 2;
+
+my $metricsdata = get($metricsurl);
+
+my %metric;
+
+#Read all those metrics into a hash
+
+my @lines = split /\n/,$metricsdata;
+
+foreach my $line (@lines) {
+	my ($key, $value)  = split(' ', $line);
+	$metric{$key}=$value;
+}
+
+my $ocskp=$metric{'cardano_node_Forge_metrics_operationalCertificateStartKESPeriod_int'} || 0;
+my $ocekp=$metric{'cardano_node_Forge_metrics_operationalCertificateExpiryKESPeriod_int'} || 0;
+my $ckp=$metric{'cardano_node_Forge_metrics_currentKESPeriod_int'} || 0;
+
+if ( $DEBUG ) {
+	print $metricsdata;
+	print "start = $ocskp\n";
+	print "end = $ocekp\n";
+	print "now = $ckp\n";
+};
+
+my $message = "UNKNOWN : NFI $ocskp $ocekp $ckp";
+my $exitcode = 2;
+
+if ( $ckp < $ocskp ) {
+	$message="CRITICAL - OPCert Not Yet Valid";
+	$exitcode=2;
+} elsif ( $ckp > $ocekp ) {
+	$message="CRITICAL - OPCert Has Expired";
+	$exitcode=2;
+} elsif ( ( $ocekp - $ckp ) < $crit ) {
+        $message = "CRITICAL - Certificate Expires Imminently $ckp / $ocekp";
+        $exitcode = 1;
+} elsif ( ( $ocekp - $ckp ) < $warn ) {
+	$message = "WARNING - Certificate Expires Soon $ckp / $ocekp";
+	$exitcode = 1;
+} elsif ( ( $ckp > $ocskp ) && ( $ckp < $ocekp ) ) {
+	$message="OK - Certificate is OK $ckp < $ocekp";
+	$exitcode=0;
+} else {
+	$message = "UNKNOWN - NFI";
+	$exitcode = 3;
+}
+
+print $message;
+exit $exitcode;
+
